2f6e75bb43e7d14824437539bd6630c32facfaa4
Daniel A Cummings <danielac@protonmail.com>
2024-09-09 12:34:11 -0600

    Send JSON payload from API to UI & make dedicated .gitignore files for UI & API

--- client/src/components/HelloWorld.vue
+++ client/src/components/HelloWorld.vue
@@ -1,43 +1,41 @@
-<script setup>
-import { ref } from 'vue'
-
-defineProps({
-  msg: String,
-})
-
-const count = ref(0)
-</script>
-
 <template>
-  <h1>{{ msg }}</h1>
-
-  <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
-    </p>
+  <div>
+    <h1>Welcome to My Vue App</h1>
+    <div v-if="loading">Loading...</div>
+    <div v-if="error">{{ error }}</div>
+    <pre v-if="data && data.message">{{ data.message }}</pre>
   </div>
-
-  <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
-  </p>
-  <p>
-    Learn more about IDE Support for Vue in the
-    <a
-      href="https://vuejs.org/guide/scaling-up/tooling.html#ide-support"
-      target="_blank"
-      >Vue Docs Scaling up Guide</a
-    >.
-  </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
 </template>
 
+<script>
+import { ref, onMounted } from 'vue';
+
+export default {
+  name: 'Home',
+  setup() {
+    const data = ref(null);
+    const loading = ref(true);
+    const error = ref(null);
+
+    onMounted(async () => {
+      try {
+        const response = await fetch('http://127.0.0.1:8000/');
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        data.value = await response.json();
+      } catch (err) {
+        error.value = err.message;
+      } finally {
+        loading.value = false;
+      }
+    });
+
+    return { data, loading, error };
+  },
+};
+</script>
+
 <style scoped>
-.read-the-docs {
-  color: #888;
-}
+/* Add any desired styles here */
 </style>
