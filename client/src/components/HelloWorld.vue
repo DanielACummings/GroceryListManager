5de74e4f3a11c6e4b43e0c5c2c507cce1721e1f2
Daniel A Cummings <danielac@protonmail.com>
2024-09-09 13:49:56 -0600

    Remove Laravel UI files & use "api" in API routes

--- client/src/components/HelloWorld.vue
+++ client/src/components/HelloWorld.vue
@@ -19,7 +19,7 @@ export default {
 
     onMounted(async () => {
       try {
-        const response = await fetch('http://127.0.0.1:8000/');
+        const response = await fetch('http://127.0.0.1:8000/api');
         if (!response.ok) {
           throw new Error('Network response was not ok');
         }
