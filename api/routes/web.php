7c72eb45bbce2fce6cc61e1de0762f15fb3e8c9a
Daniel A Cummings <danielac@protonmail.com>
2024-09-20 10:58:00 -0600

    Connect existing UI to new Breeze API & change from sqlite to mysql

--- api/routes/web.php
+++ api/routes/web.php
@@ -1,7 +1,20 @@
 <?php
 
+use App\Http\Controllers\ItemListController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+$baseRoute = 'api';
+
+Route::get("{$baseRoute}/", function () {
+    return response()->json(['message' => 'UI is connected to API']);
 });
+// Route::get("{$baseRoute}/lists", [ItemListController::class, 'index']);
+// Route::post("{$baseRoute}/lists", [ItemListController::class, 'store']);
+// Route::get("{$baseRoute}/sanctum/csrf-cookie", function () {
+//     return response() ->json(['token' => csrf_token()]);
+// })
+// ->middleware('web');
+
+// Route::post("{$baseRoute}/login", [UserController::class, 'login']);
+// Route::post("{$baseRoute}/register", [UserController::class, 'register']);
